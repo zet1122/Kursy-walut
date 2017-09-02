e1c79c447f35d19d481bf4165104865f241929f9
zet1122 <pawzlk12@gmail.com>
2017-09-02 20:23:13 +0200

    Drobne poprawki

--- CurrencyRates/MainWindow.xaml.cs
+++ CurrencyRates/MainWindow.xaml.cs
@@ -21,16 +21,26 @@ namespace CurrencyRates
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Currency> CurrencyList = new List<Currency>();
+        
         public MainWindow()
         {
             InitializeComponent();
-            showChart();
+            InitializeChart();
         }
 
+        private void InitializeBindings()
+        {
+            chooseCurrency.ItemsSource = CurrencyList;
+            chooseCurrency.DisplayMemberPath = "Code";
+            singleCurrencyView.ItemsSource = CurrencyList;
+            nowLabel.Content = "-- Aktualne średnie kursy: " + DateTime.Now.ToShortDateString() + " --";
+        }
+        
         private void downloadBtn_Click(object sender, RoutedEventArgs e)
         {
             Download.listOfCurrency.Clear();
-            
+
             try
             {
                 Download.DownloadData();
@@ -40,18 +50,17 @@ namespace CurrencyRates
                 MessageBox.Show("Błąd podczas pobierania danych: " + ex.Message, "Currency Rates", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
 
-            chooseCurrency.ItemsSource = Download.listOfCurrency;
-            singleCurrencyView.ItemsSource = Download.listOfCurrency;
-            nowLabel.Content = "-- Aktualne średnie kursy: " + DateTime.Now.ToShortDateString() +" --";
+            CurrencyList = Download.listOfCurrency;
+            InitializeBindings();
         }
-        
+
         private Dictionary<string, double> getChartData(List<Currency> lst, string str)
         {
             int index = 0;
-            
+
             switch (str)
             {
-                case "USD": 
+                case "USD":
                     index = 0;
                     break;
                 case "EUR":
@@ -66,37 +75,28 @@ namespace CurrencyRates
                 default: break;
             }
 
-            return new Dictionary<string, double>(lst[index].Exchange);         
-        }
-
-        private void chooseCurrency_Loaded(object sender, RoutedEventArgs e)
-        {           
-            chooseCurrency.DisplayMemberPath = "Code";
-            chooseCurrency.SelectedIndex = 0;            
+            return new Dictionary<string, double>(lst[index].Exchange);
         }
 
         private void chooseCurrency_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //chooseCurrency = sender as ComboBox;
-            
             string value = chooseCurrency.SelectedItem.ToString();
-            Dictionary<string, double> chartData = new Dictionary<string, double>(getChartData(Download.listOfCurrency, value));
-            
-            
+            Dictionary<string, double> chartData = new Dictionary<string, double>(getChartData(CurrencyList, value));
+
             allCurrencyView.ItemsSource = chartData;
             lineChart.Title = value;
-            showLineChart(chartData);                      
+            showLineChart(chartData);
         }
 
         private void showLineChart(Dictionary<string, double> dict)
         {
             List<KeyValuePair<string, double>> valueList = new List<KeyValuePair<string, double>>();
-            
+
             foreach (var item in dict)
             {
                 valueList.Add(item);
             }
-            
+
             valueList.Reverse();
             lineChart.DataContext = valueList;
         }
@@ -105,23 +105,23 @@ namespace CurrencyRates
         {
             string fileName = DateTime.Now.ToShortDateString() + " " + DateTime.Now.Hour + DateTime.Now.Minute + ".txt";
             string date = "Utworzono: " + DateTime.Now.ToString();
-            
-            File.WriteAllText(fileName, date + Environment.NewLine);            
 
-            foreach (var item in Download.listOfCurrency)
+            File.WriteAllText(fileName, date + Environment.NewLine);
+
+            foreach (var item in CurrencyList)
             {
                 var codeName = item.Code;
                 var rank = item.Exchange;
 
-                File.AppendAllText(fileName, codeName + Environment.NewLine);                                
+                File.AppendAllText(fileName, codeName + Environment.NewLine);
                 File.AppendAllLines(fileName, rank.Select(x => x.ToString()));
             }
 
             MessageBox.Show("Zapis się udał", "Currency Rates", MessageBoxButton.OK, MessageBoxImage.Information);
-            
+
         }
 
-        private void showChart()
+        private void InitializeChart()
         {
             List<KeyValuePair<string, double>> MyValue = new List<KeyValuePair<string, double>>();
             MyValue.Add(new KeyValuePair<string, double>("Data", 0.0));
